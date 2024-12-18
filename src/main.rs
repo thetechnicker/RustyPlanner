826a08be0dd8ef10b7e29cb64e6986ef9d4f0b3e
Thetecknicker <106349149+thetechnicker@users.noreply.github.com>
2024-12-18 12:48:22 +0100

    reading args and stdin

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::io;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    // Get command-line arguments
+    let args: Vec<String> = env::args().collect();
+    println!("Command-line arguments:");
+    for arg in args {
+        println!("{}", arg);
+    }
+
+    // Read from standard input
+    let mut input = String::new();
+    println!("Please enter some input:");
+
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+
+    println!("You entered: {}", input.trim());
+
 }
+
