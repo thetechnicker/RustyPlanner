14958b8c60ed8d49161e8a652a5428c5474562c9
Thetecknicker <106349149+thetechnicker@users.noreply.github.com>
2025-01-07 11:35:46 +0100

    added: command mode

--- src/main.rs
+++ src/main.rs
@@ -5,21 +5,14 @@ use std::io::{self, Write};
 use directories::BaseDirs;
 use std::fs;
 use std::path::PathBuf;
+use std::env;
 //use notify_rust::Notification;
 
 
 
 fn main() {
-    //let mut events: Vec<Event> = Vec::new();
-
-    /*
-    Notification::new()
-        .summary("Firefox News")
-        .body("This will almost look like a real firefox notification.")
-        .icon("firefox")
-        .show();
-    */
-
+    let args: Vec<String> = env::args().collect();
+    
     let data_file_path: Option<PathBuf>;
 
     if let Some(base_dirs) = BaseDirs::new() {
@@ -49,6 +42,25 @@ fn main() {
 
     event_manager.read_events_from_file();
 
+    if args.len() > 1 {
+         command_mode(&mut event_manager, &args[1..]);
+    } else {
+        loop_mode(&event_manager);
+    }
+}
+
+fn loop_mode(event_manager: &mut EventManager){
+    //let mut events: Vec<Event> = Vec::new();
+
+    /*
+       Notification::new()
+       .summary("Firefox News")
+       .body("This will almost look like a real firefox notification.")
+       .icon("firefox")
+       .show();
+       */
+
+
     loop {
         let mut input = String::new();
 
@@ -74,18 +86,20 @@ fn main() {
 
         if trimmed.to_lowercase().starts_with("add") {
             event_manager.add_event_from_str(trimmed);
-            /*
-               match event {
-               Some(e) => {
-               println!("event name: {}", e.name);
-               println!("event datetime: {}", e.timedate);
-               events.push(e);
-               },
-               None => println!("event couldnt be parsed!"),
-               }
-               */
-            //save_events(&data_file_path, &events);
         }
     }
 }
 
+fn command_mode(event_manager: &mut EventManager, commands: &[String]) {
+    let command = commands.join(" ");
+    
+    if command.starts_with("add") {
+        event_manager.add_event_from_str(&command);
+    } else if command == "list" {
+        event_manager.list_events();
+    } else if command == "clear" {
+        event_manager.clear();
+    } else {
+        eprintln!("Unknown command: {}", command);
+    }
+}
