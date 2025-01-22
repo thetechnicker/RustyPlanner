92539b88a6cc6a6e844da75555a0c77838d50b98
Thetecknicker <106349149+thetechnicker@users.noreply.github.com>
2025-01-22 12:48:31 +0100

    idk something about fmt

--- src/background_service.rs
+++ src/background_service.rs
@@ -11,8 +11,8 @@ use std::fs::File;
 use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::Duration;
-use uzers::{get_current_gid, get_current_uid};
 use utils::get_path;
+use uzers::{get_current_gid, get_current_uid};
 
 use signal_hook::flag;
 use std::io::Error;
