f09e24fe0af58f0dddfddc977c2d12f2c90a9aa9
thetechnicker <106349149+thetechnicker@users.noreply.github.com>
2025-02-09 15:22:24 +0100

    moved help msg in own file to reduce length of main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,17 @@ mod miscs;
 use chrono::DateTime;
 use chrono::Local;
 use events::{
-    event::{
-        load_categories, save_categories, Attendee, Event, Notification, NotificationMethod,
-        ATTENDEE_FIELDS, EVENT_FIELDS, RECURRENCE_FIELDS,
-    },
+    event::{load_categories, save_categories, Attendee, Event, Notification, NotificationMethod},
     event_manager::{EventManager, EventManagerMode},
 };
-use miscs::arg_parsing::parse_data;
-use miscs::utils::{clear_screen, get_path};
-use miscs::utils::{date_from_str, time_from_str};
+use miscs::{
+    arg_parsing::parse_data,
+    help::{
+        print_add_help, print_clear_help, print_edit_help, print_help, print_list_help,
+        print_remove_help, print_save_help,
+    },
+    utils::{clear_screen, date_from_str, get_path, time_from_str},
+};
 use std::env;
 use std::fs;
 use std::io::{self, Write};
@@ -309,86 +311,6 @@ fn ask_user(prompt: &str, default: &str) -> String {
         trimmed.to_string()
     }
 }
-
-fn print_help() {
-    println!("Available commands:");
-    println!("  add    - Add a new event");
-    println!("  save   - Save events to file");
-    println!("  remove - Remove an event by index");
-    println!("  edit   - Edit an event by index");
-    println!("  cls    - Clear the screen");
-    println!("  list   - List all events");
-    println!("  clear  - Clear all events");
-    println!("  help   - Show this help message");
-    println!("  exit   - Exit the application");
-    println!();
-    //println!("Use the 'add' command to create a new event with optional parameters for description, location, and notification time.");
-    println!(
-        "For more details on each command run `help [Command Name]`, refer to the documentation."
-    );
-}
-
-fn print_add_help() {
-    let mut help_message = "Usage: add event [OPTIONS]\n\n".to_string();
-    help_message += "event:\n";
-    for attribute in EVENT_FIELDS.iter() {
-        let part_a = format!("\t{}:", attribute[0]);
-        let part_b = format!("\t[{}]", attribute[1]);
-        help_message += &format!("{:<20}{}\n", part_a, part_b);
-        if attribute[0] == "recurrence" {
-            help_message += "\t\tRecurrence Attributes:\n";
-            for recurrence_attribute in RECURRENCE_FIELDS.iter() {
-                let part_a = format!("\t{}:", recurrence_attribute[0]);
-                let part_b = format!("\t[{}]", recurrence_attribute[1]);
-                help_message += &format!("\t\t{:<20}{}\n", part_a, part_b);
-            }
-        }
-        if attribute[0] == "attendees" {
-            help_message += "\tAttendee Attributes:\n";
-            for attendee_attributes in ATTENDEE_FIELDS.iter() {
-                let part_a = format!("\t{}:", attendee_attributes[0]);
-                let part_b = format!("\t[{}]", attendee_attributes[1]);
-                help_message += &format!("\t\t{:<20}{}\n", part_a, part_b);
-            }
-        }
-    }
-    println!("{}", help_message);
-}
-
-fn print_save_help() {
-    println!("  save           - Save events to file");
-    println!("                  Usage: save <filename>");
-    println!("                  Description: Saves all current events to the specified file.");
-}
-
-fn print_remove_help() {
-    println!("  remove <index> - Remove an event by index");
-    println!("                  Usage: remove <index>");
-    println!("                  Description: Removes the event at the specified index from the list of events.");
-}
-
-fn print_edit_help() {
-    println!("  edit <index>   - Edit an event by index");
-    println!("                  Usage: edit <index> [options]");
-    println!("                  Description: Edits the event at the specified index. Options can include");
-    println!("                  mode, name, date, time, description, location, and alarm time.");
-}
-
-fn print_cls_help() {
-    println!("  cls            - Clear the screen");
-    println!("                  Description: Clears the console screen for better visibility.");
-}
-
-fn print_list_help() {
-    println!("  list           - List all events");
-    println!("                  Description: Displays all current events in the calendar.");
-}
-
-fn print_clear_help() {
-    println!("  clear          - Clear all events");
-    println!("                  Description: Removes all events from the calendar.");
-}
-
 fn update_event(event: &mut Event) {
     // Update title
     let new_title = ask_user("Enter new title", &event.title);
--- src/miscs/mod.rs
+++ src/miscs/mod.rs
@@ -1,6 +1,8 @@
 #[allow(dead_code)]
 pub mod arg_parsing;
 #[allow(dead_code)]
+pub mod help;
+#[allow(dead_code)]
 pub mod notification;
 #[allow(dead_code)]
 pub mod utils;
