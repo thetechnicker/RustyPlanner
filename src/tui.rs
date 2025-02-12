cf23860f839e5bdbc2e56e5bb229afacb5138279
Thetecknicker <106349149+thetechnicker@users.noreply.github.com>
2025-02-12 10:18:43 +0100

    highliting active element

--- src/tui.rs
+++ src/tui.rs
@@ -12,6 +12,7 @@ use ratatui::{
     crossterm::{
         event::{
             self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode, KeyEvent, KeyEventKind,
+            MouseEvent, MouseEventKind,
         },
         execute,
         terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
@@ -44,8 +45,24 @@ fn main() -> io::Result<()> {
         DisableMouseCapture
     )?;
     ratatui::restore();
-    println!("{}", app.search_term);
     app_result
+}
+
+#[derive(Debug, PartialEq, Eq)]
+enum Selected {
+    None,
+    Search,
+    Tabel,
+}
+
+impl Selected {
+    fn next(&self) -> Self {
+        match self {
+            Self::None => Self::Search,
+            Self::Search => Self::Tabel,
+            Self::Tabel => Self::None,
+        }
+    }
 }
 
 #[derive(Debug)]
@@ -54,6 +71,8 @@ pub struct App {
     exit: bool,
     pub search_term: String,
     last_tick: Instant,
+    selected: Selected,
+    size: Rect,
 }
 
 impl Default for App {
@@ -63,12 +82,20 @@ impl Default for App {
             exit: false,
             scroll: 0,
             last_tick: Instant::now(),
+            selected: Selected::None,
+            size: Rect {
+                x: 0,
+                y: 0,
+                width: 0,
+                height: 0,
+            },
         }
     }
 }
 
 impl App {
-    const TICK_RATE: Duration = Duration::from_millis(250);
+    const TICK_RATE: Duration = Duration::from_millis(1);
+    const MAX_SCROLL: u16 = 10;
 
     /// runs the application's main loop until the user quits
     pub fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
@@ -82,7 +109,7 @@ impl App {
         Ok(())
     }
 
-    fn draw(&self, frame: &mut Frame) {
+    fn draw(&mut self, frame: &mut Frame) {
         frame.render_widget(self, frame.area());
     }
 
@@ -95,17 +122,52 @@ impl App {
                 Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
                     self.handle_key_event(key_event)
                 }
-                //Event::Mouse(mouse) => {}
+                Event::Mouse(mouse) => self.handle_mouse_event(mouse),
                 _ => {}
             };
         }
         Ok(())
     }
 
+    fn handle_mouse_event(&mut self, mouse: MouseEvent) {
+        match mouse.kind {
+            MouseEventKind::Down(_) => {
+                if self.size.x <= mouse.column
+                    && self.size.width >= mouse.column
+                    && self.size.y <= mouse.row
+                    && self.size.height >= mouse.row
+                {
+                    if self.size.y <= mouse.row && mouse.row <= 3 {
+                        self.selected = Selected::Search;
+                    } else if mouse.row > 3 && mouse.row <= self.size.height - 5 {
+                        self.selected = Selected::Tabel
+                    } else if mouse.row > self.size.height - 5 {
+                        self.selected = Selected::None
+                    }
+                }
+            }
+            MouseEventKind::Moved => {}
+            MouseEventKind::ScrollUp => self.scroll = (self.scroll + 1) % (Self::MAX_SCROLL + 1),
+            MouseEventKind::ScrollDown => {
+                if self.scroll as i16 - 1 < 0 {
+                    self.scroll = Self::MAX_SCROLL;
+                } else {
+                    self.scroll -= 1;
+                }
+            }
+            _ => {}
+        }
+    }
+
     fn handle_key_event(&mut self, key_event: KeyEvent) {
         match key_event.code {
-            KeyCode::Char('q') => self.exit(),
-            KeyCode::Tab => todo!(),
+            KeyCode::Char('q') if self.selected == Selected::None => self.exit(),
+            KeyCode::Char(x) if self.selected == Selected::Search => self.search_term.push(x),
+            KeyCode::Backspace if self.selected == Selected::Search => {
+                self.search_term.pop();
+            }
+            KeyCode::Tab => self.selected = self.selected.next(),
+            KeyCode::Esc => self.selected = Selected::None,
             _ => {}
         }
     }
@@ -115,22 +177,29 @@ impl App {
     }
 }
 
-impl Widget for &App {
+impl Widget for &mut App {
     fn render(self, area: Rect, buf: &mut Buffer) {
         let title = Line::from("Rusty Planner".bold());
-        let instructions = Line::from(vec![" Quit ".into(), "<Q> ".blue().bold()]);
+        let instructions = Line::from(vec![
+            " Currently Active ".into(),
+            format!("{:?} ", self.selected).blue().bold(),
+            " Quit ".into(),
+            "<Q> ".blue().bold(),
+        ]);
         let block = Block::bordered()
             .title(title.centered())
             .title_bottom(instructions.centered())
             .border_set(border::THICK)
             .border_type(BorderType::Rounded);
 
+        self.size = block.inner(area);
+
         let layout = Layout::default()
             .direction(Direction::Vertical)
             .constraints(vec![
                 Constraint::Length(3),
                 Constraint::Min(10),
-                Constraint::Length(3),
+                Constraint::Length(5),
             ])
             .split(block.inner(area));
 
@@ -138,10 +207,26 @@ impl Widget for &App {
             "Search: ".into(),
             self.search_term.clone().white(),
         ])]))
-        .block(Block::bordered().border_type(BorderType::Rounded));
+        .block(
+            Block::bordered()
+                .border_type(BorderType::Rounded)
+                .border_style(Style::default().fg(if self.selected == Selected::Search {
+                    Color::Green
+                } else {
+                    Color::White
+                })),
+        );
 
         let calender = Paragraph::new("NOTHING")
-            .block(Block::bordered().border_type(BorderType::Rounded))
+            .block(
+                Block::bordered()
+                    .border_type(BorderType::Rounded)
+                    .border_style(Style::default().fg(if self.selected == Selected::Tabel {
+                        Color::Green
+                    } else {
+                        Color::White
+                    })),
+            )
             .scroll((self.scroll, 0))
             .wrap(Wrap { trim: true });
 
